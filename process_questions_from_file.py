0236fc54f3a69bf5df90ca76c61758d21716a4ff
DataBass <johnsmith@agentforge.net>
2024-09-29 19:46:20 -0500

    Initial file handling poc

---
+++ process_questions_from_file.py
@@ -0,0 +1,50 @@
+from Modules.TrinityLoop import Trinity
+from Utilities.Memory import Memory
+from agentforge.utils.guiutils.discord_client import DiscordClient
+import yaml
+
+
+class Gen_From_File:
+
+    def __init__(self, file_path):
+        self.path = file_path
+        with open(".agentforge/personas/default.yaml", "r") as file:
+            self.persona = yaml.safe_load(file)
+            self.persona_name = self.persona.get("Name")
+        self.memory = Memory(self.persona, self.persona_name)
+        self.discord = DiscordClient()
+        self.trinity = Trinity(self.memory, self.discord)
+
+    def run_file(self):
+        try:
+            with open(self.path, 'r') as file:
+                lines = file.readlines()
+                print(f'File open:\n{lines}')
+            
+                for index, line in enumerate(lines, start=1):
+                    print(f"{index}. {line.strip()}")
+                    message = {
+                        'channel': 'general',
+                        'channel_id': 1287528589251711098,
+                        'message': line,
+                        'message_id': 1290104562493161527,
+                        'author': 'Ansel',
+                        'timestamp': '2024-09-30 00:14:56',
+                        }
+                    self.trinity.do_chat(message)
+        
+        except FileNotFoundError:
+            print(f"Error: The file '{self.path}' was not found.")
+        except IOError:
+            print(f"Error: There was an issue reading the file '{self.path}'.")
+
+
+# Example usage
+if __name__ == "__main__":
+    file_path = "text.txt"  # Replace with the actual path to your file
+    print('init gen')
+    generator = Gen_From_File(file_path)
+    print('run gen')
+    generator.run_file()
+    print('gen finished')
+
